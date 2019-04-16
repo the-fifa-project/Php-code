b5aa0f6ac5125dd43bc5398d260e7263c664f1e5
Glenn <glenn.dev@hotmail.com>
2019-04-16 14:08:35 +0200

    maked the html form

--- includes/controller.php
+++ includes/controller.php
@@ -5,3 +5,24 @@
  * Date: 16-4-2019
  * Time: 12:53
  */
+
+require 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST' )
+{
+    header("location: ..//index.php");
+    exit;
+}
+
+if ($_POST['type'] === 'register')
+{
+    $firstName = htmlentities($_POST['firstname']);
+    $middleName = htmlentities($_POST['middlename']);
+    $lastName = htmlentities($_POST['lastname']);
+    $email = htmlentities($_POST['email']);
+    $password = htmlentities($_POST['password']);
+
+    // check of it is empty
+
+
+}
--- register.php
+++ register.php
@@ -13,7 +13,13 @@ require 'header.php';
 
     <main>
         <form action="includes/controller.php" method="post">
-
+            <input type="hidden" name="type" value="register">
+            <input type="text" name="firstname" required>
+            <input type="text" name="middlename">
+            <input type="text" name="lastname" required>
+            <input type="email" name="email" required>
+            <input type="password" name="password" required>
+            <input type="submit" name="submit" value="register">
         </form>
     </main>
 
